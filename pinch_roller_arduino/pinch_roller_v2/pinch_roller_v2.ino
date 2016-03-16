10510d4efdbca8f5300787aa9e70f28331b82d14
Nic <nseymoursmith@gmail.com>
2016-03-16 12:44:35 +0000

    Changed arduino to just receive a speed for the motor

---
+++ pinch_roller_arduino/pinch_roller_v2/pinch_roller_v2.ino
@@ -0,0 +1,40 @@
+//Pinch roller motor driver by N. Seymour-Smith
+//This version offloads feedback calculations to the microscope computer
+
+//Digital Pins
+int motor_spoolerPin = 5;     //PWM pin for spooler motor
+
+//Variables for puller speed control
+int puller_speed = 125;
+
+
+void setup ()
+{
+  Serial.begin (9600);
+  while(!Serial);
+  Serial.setTimeout(10);
+  pinMode(motor_spoolerPin, OUTPUT);
+}
+
+void loop ()
+{
+  serial_check();
+  pull_control();       //Go to the spool_control function for auto control
+
+}
+  
+   
+void serial_check()
+{
+  if(Serial.available())
+  {
+   puller_speed = Serial.parseInt();   
+  }
+}
+   
+void pull_control()
+{
+  analogWrite(motor_spoolerPin,puller_speed);   //Set the spool speed to the PID result
+}
+
+
